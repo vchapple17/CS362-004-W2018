19b326ede687154c48bcfa53f06d82f8bb9da955
vchapple17 <vchapple17@gmail.com>
2018-01-23 21:36:45 -0600

    isGameOver Unit Test

---
+++ projects/chapplev/dominion/unittest1.c
@@ -0,0 +1,159 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include "dominion.h"
+#include "dominion_helpers.h"
+
+/* Unit Test #1 Dominion */
+
+/*
+
+Test the function:
+
+int isGameOver(struct gameState *state)
+
+*/
+
+int myAssert(int conditional, char * name, char * T, char * F) {
+  if (conditional) {
+    printf("%s\t%s\n", T, name);
+  }
+  else {
+    printf("%s\t%s\n", F, name);
+  }
+  return 0;
+}
+// struct gameState {
+//   int numPlayers; //number of players
+//   int supplyCount[treasure_map+1];  //this is the amount of a specific type of card given a specific number.
+//   int embargoTokens[treasure_map+1];
+//   int outpostPlayed;
+//   int outpostTurn;
+//   int whoseTurn;
+//   int phase;
+//   int numActions; /* Starts at 1 each turn */
+//   int coins; /* Use as you see fit! */
+//   int numBuys; /* Starts at 1 each turn */
+//   int hand[MAX_PLAYERS][MAX_HAND];
+//   int handCount[MAX_PLAYERS];
+//   int deck[MAX_PLAYERS][MAX_DECK];
+//   int deckCount[MAX_PLAYERS];
+//   int discard[MAX_PLAYERS][MAX_DECK];
+//   int discardCount[MAX_PLAYERS];
+//   int playedCards[MAX_DECK];
+//   int playedCardCount;
+// };
+
+//
+// int testIsGameOver(struct gameState *gamePre) {
+//
+//
+//   // Call isGameOverFunction
+//
+//
+// }
+
+
+int main(int argc, char *argv[]) {
+  // Kingdom Cards
+  int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy};
+
+  // Non-Kingdom cards excluding "province"
+  int nonKingdom[6] = {curse, estate, duchy, copper, silver, gold};
+
+  // Setup gameStates
+  struct gameState game;
+  struct gameState gamePre;
+  struct gameState gamePost;
+  int result = -1;
+  int expected = -1;
+  int i = 0;
+
+  initializeGame(2, k, 1, &game);
+
+  // Set Num Players and counts
+  game.numPlayers = 2;
+  for( i = 0; i < game.numPlayers; i++ ) {
+    game.deckCount[i] =  1 * MAX_DECK;
+    game.discardCount[i] = 1 * MAX_DECK;
+    game.handCount[i] = 1 *  MAX_HAND;
+    memset(game.hand[i], 0, sizeof(int) * MAX_HAND); // From testDrawCard.c
+  }
+
+  // TEST #1
+  char * test = "Returns 1 for empty Province stack.";
+  memcpy(&gamePre, &game, sizeof(struct gameState));  // Set PRE
+  printf("\n\n======TEST: %s======\n", test );
+  gamePre.supplyCount[province] = 0;   // Force Province stack to zero
+  memcpy(&gamePost, &gamePre, sizeof(struct gameState)); // Set POST (testcase)
+  expected = 1;   // Expect that game is over
+  result = isGameOver(&gamePost);
+  myAssert( (result == expected) , "Return Value", "PASS", "FAIL");
+  myAssert( (memcmp(&gamePre, &gamePost, sizeof(struct gameState)) == 0), "Same Game State", "PASS", "FAIL");
+
+  // TEST #2
+  test = "Returns 0 for non-empty Province stack.";
+  memcpy(&gamePre, &game, sizeof(struct gameState));  // Set PRE
+  printf("\n\n======TEST: %s======\n", test );
+  gamePre.supplyCount[province] = 1;   // Force Province stack to 1
+  memcpy(&gamePost, &gamePre, sizeof(struct gameState)); // Set POST (testcase)
+  expected = 0;   // Expect that game is NOT over
+  result = isGameOver(&gamePost);
+  myAssert( (result == expected) , "Return Value", "PASS", "FAIL");
+  myAssert( (memcmp(&gamePre, &gamePost, sizeof(struct gameState)) == 0), "Same Game State", "PASS", "FAIL");
+
+  // TEST #3
+  test = "Returns 1 for 3 empty (non-province) supply piles.";
+  memcpy(&gamePre, &game, sizeof(struct gameState));  // Set PRE
+  printf("\n\n======TEST: %s======\n", test );
+  gamePre.supplyCount[nonKingdom[0]] = 0;   // Force 3 supply piles to be zero
+  gamePre.supplyCount[nonKingdom[1]] = 0;
+  gamePre.supplyCount[nonKingdom[2]] = 0;
+  memcpy(&gamePost, &gamePre, sizeof(struct gameState)); // Set POST (testcase)
+  expected = 1;   // Expect that game is over
+  result = isGameOver(&gamePost);
+  myAssert( (result == expected) , "Return Value", "PASS", "FAIL");
+  myAssert( (memcmp(&gamePre, &gamePost, sizeof(struct gameState)) == 0), "Same Game State", "PASS", "FAIL");
+
+  // TEST #4
+  test = "Returns 0 for 2 empty (non-province) supply piles.";
+  memcpy(&gamePre, &game, sizeof(struct gameState));  // Set PRE
+  printf("\n\n======TEST: %s======\n", test );
+  gamePre.supplyCount[nonKingdom[0]] = 0;   // Exactly 2 supply piles are zero
+  gamePre.supplyCount[nonKingdom[1]] = 0;
+  memcpy(&gamePost, &gamePre, sizeof(struct gameState)); // Set POST (testcase)
+  expected = 0;   // Expect that game is NOT over
+  result = isGameOver(&gamePost);
+  myAssert( (result == expected) , "Return Value", "PASS", "FAIL");
+  myAssert( (memcmp(&gamePre, &gamePost, sizeof(struct gameState)) == 0), "Same Game State", "PASS", "FAIL");
+
+  // TEST #5
+  test = "Returns 0 for 1 empty (non-province) supply piles.";
+  memcpy(&gamePre, &game, sizeof(struct gameState));  // Set PRE
+  printf("\n\n======TEST: %s======\n", test );
+  gamePre.supplyCount[nonKingdom[0]] = 0;   // Exactly 1 supply pile is zero
+  memcpy(&gamePost, &gamePre, sizeof(struct gameState)); // Set POST (testcase)
+  expected = 0;   // Expect that game is NOT over
+  result = isGameOver(&gamePost);
+  myAssert( (result == expected) , "Return Value", "PASS", "FAIL");
+  myAssert( (memcmp(&gamePre, &gamePost, sizeof(struct gameState)) == 0), "Same Game State", "PASS", "FAIL");
+
+  // TEST #6
+  test = "Returns 1 for 4 empty (non-province) supply piles.";
+  memcpy(&gamePre, &game, sizeof(struct gameState));  // Set PRE
+  printf("\n\n======TEST: %s======\n", test );
+  gamePre.supplyCount[nonKingdom[0]] = 0;   // Force 3 supply piles to be zero
+  gamePre.supplyCount[nonKingdom[1]] = 0;
+  gamePre.supplyCount[nonKingdom[2]] = 0;
+  gamePre.supplyCount[nonKingdom[3]] = 0;
+  memcpy(&gamePost, &gamePre, sizeof(struct gameState)); // Set POST (testcase)
+  expected = 1;   // Expect that game is over
+  result = isGameOver(&gamePost);
+  myAssert( (result == expected) , "Return Value", "PASS", "FAIL");
+  myAssert( (memcmp(&gamePre, &gamePost, sizeof(struct gameState)) == 0), "Same Game State", "PASS", "FAIL");
+
+  printf("\n\n");
+
+  return 0;
+}
