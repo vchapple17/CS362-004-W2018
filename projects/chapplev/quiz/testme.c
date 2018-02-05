3b034f65457aa2666bb79995b3474a5829654264
vchapple17 <vchapple17@gmail.com>
2018-02-04 19:14:08 -0800

    makefile and testme.c

---
+++ projects/chapplev/quiz/testme.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+
+#define STRING_LENGTH 5
+#define NUM_CHARS 9
+#define NUM_CHARS2 4
+char CHARS[] = {'[','(','{', ' ', 'a', 'x', '}', ')', ']' };
+char CHARS2[] = {'r','s','e', 't'};
+char * string;
+
+char inputChar() {
+    // TODO: rewrite this function
+    // Pick random character
+    int index = rand() % ( NUM_CHARS);
+    return CHARS[index];
+}
+
+char *inputString() {
+
+  int i = 0;
+  memset(string, '\0', STRING_LENGTH + 1);
+
+  for (i = 0; i < (STRING_LENGTH); i++) {
+      string[i] = CHARS2[ rand() % NUM_CHARS2 ];
+  }
+
+  return string;
+}
+
+void testme()
+{
+  int tcCount = 0;
+  char *s;
+  char c;
+  int state = 0;
+  while (1) {
+    tcCount++;
+    c = inputChar();
+    s = inputString();
+    printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
+
+    if (c == '[' && state == 0) state = 1;
+    if (c == '(' && state == 1) state = 2;
+    if (c == '{' && state == 2) state = 3;
+    if (c == ' ' && state == 3) state = 4;
+    if (c == 'a' && state == 4) state = 5;
+    if (c == 'x' && state == 5) state = 6;
+    if (c == '}' && state == 6) state = 7;
+    if (c == ')' && state == 7) state = 8;
+    if (c == ']' && state == 8) state = 9;
+    if (s[0] == 'r' && s[1] == 'e'
+       && s[2] == 's' && s[3] == 'e'
+       && s[4] == 't' && s[5] == '\0'
+       && state == 9) {
+      printf("error ");
+      exit(200);
+    }
+
+  }
+}
+
+
+int main(int argc, char *argv[])
+{
+    string = (char *) malloc(sizeof(char *) * (STRING_LENGTH + 1));
+    srand(time(NULL));
+    testme();
+    free(string);
+    return 0;
+}
